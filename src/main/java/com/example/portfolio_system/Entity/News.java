d2cf173c0ced97ff690975e840d458cb6fa38a6d
ThisIsNana <pw10279@gmail.com>
2023-08-22 13:47:16 +0800

    變更java11, 設定SB 2.7.10, 新增sql檔及Entity

---
+++ src/main/java/com/example/portfolio_system/Entity/News.java
@@ -0,0 +1,118 @@
+package com.example.portfolio_system.Entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "news")
+public class News {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "news_id")
+	private int newsId;
+
+	@Column(name = "news_title")
+	private String newsTitle;
+
+	@Column(name = "news_create_date")
+	private LocalDate newsCreateDate;
+
+	@Column(name = "news_update_date")
+	private LocalDate newsUpdateDate;
+
+	@Column(name = "news_category")
+	private String newsCategory;
+
+	@Column(name = "news_description")
+	private String newsDescription;
+
+	@Column(name = "news_reading_count")
+	private int newsReadingCount;
+	
+//	=========================================
+
+	public News() {
+		super();
+	}
+
+	public News(int newsId, String newsTitle, LocalDate newsCreateDate, 
+			LocalDate newsUpdateDate, String newsCategory,
+			String newsDescription, int newsReadingCount) {
+	super();
+	this.newsId = newsId;
+	this.newsTitle = newsTitle;
+	this.newsCreateDate = newsCreateDate;
+	this.newsUpdateDate = newsUpdateDate;
+	this.newsCategory = newsCategory;
+	this.newsDescription = newsDescription;
+	this.newsReadingCount = newsReadingCount;
+}
+
+//	=========================================
+
+
+	public int getNewsId() {
+		return newsId;
+	}
+
+	public void setNewsId(int newsId) {
+		this.newsId = newsId;
+	}
+
+	public String getNewsTitle() {
+		return newsTitle;
+	}
+
+	public void setNewsTitle(String newsTitle) {
+		this.newsTitle = newsTitle;
+	}
+
+	public LocalDate getNewsCreateDate() {
+		return newsCreateDate;
+	}
+
+	public void setNewsCreateDate(LocalDate newsCreateDate) {
+		this.newsCreateDate = newsCreateDate;
+	}
+
+	public LocalDate getNewsUpdateDate() {
+		return newsUpdateDate;
+	}
+
+	public void setNewsUpdateDate(LocalDate newsUpdateDate) {
+		this.newsUpdateDate = newsUpdateDate;
+	}
+
+	public String getNewsCategory() {
+		return newsCategory;
+	}
+
+	public void setNewsCategory(String newsCategory) {
+		this.newsCategory = newsCategory;
+	}
+
+	public String getNewsDescription() {
+		return newsDescription;
+	}
+
+	public void setNewsDescription(String newsDescription) {
+		this.newsDescription = newsDescription;
+	}
+
+	public int getNewsReadingCount() {
+		return newsReadingCount;
+	}
+
+	public void setNewsReadingCount(int newsReadingCount) {
+		this.newsReadingCount = newsReadingCount;
+	}
+	
+	
+}
---
+++ src/main/resources/portfolio_system.sql
@@ -0,0 +1,14 @@
+
+#建立資料庫
+CREATE SCHEMA IF NOT EXISTS `portfolio_system` ;
+
+#建立[最新消息]表格
+CREATE TABLE IF NOT EXISTS `portfolio_system`.`news` (
+  `news_id` INT NOT NULL AUTO_INCREMENT,
+  `news_title` VARCHAR(200) NOT NULL,
+  `news_create_date` DATE NOT NULL,
+  `news_update_date` DATE NOT NULL,
+  `news_category` VARCHAR(200) NOT NULL,
+  `news_description` VARCHAR(2000) NOT NULL,
+  `news_reading_count` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`news_id`));
